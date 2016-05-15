43d0bfff405adda993809ca1944942e36a2a3721
madhav-datt <madhav.datt@hotmail.com>
2016-05-15 22:25:36 +0530

    add login credentials storage and use

--- src/authenticate.py
+++ src/authenticate.py
@@ -1,9 +1,45 @@
 #
 # authenticate.py
 #
-# Create and use login credentials for clmail
+# Store and use login credentials for clmail
 #
 # Copyright (C)   2016    Madhav Datt
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #
 
+import keyring
+import keyring.errors
+
+
+def connect_email_account(email, password):
+    """
+    Securely store password in Python keyring
+
+    :param email: Email address for account to be used with clmail
+    :param password: Password for email address
+    """
+    # Throws keyring.errors.PasswordSetError
+    keyring.set_password("clmail", email, password)
+
+
+def get_password(email):
+    """
+    :param email: Email address for account associated with clmail
+    :return: Password for email address
+    """
+
+    # None if password doesn't exist
+    return keyring.get_password("clmail", email)
+
+
+def remove_email(email):
+    """
+    Remove stored email and password from keyring
+
+    :param email: Email address for account associated with clmail
+    """
+
+    try:
+        keyring.delete_password("clmail", email)
+    except keyring.errors.PasswordDeleteError:
+        pass
