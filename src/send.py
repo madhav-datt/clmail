bcb6dd471e4347dd558fd0165f8a2e790bca3273
madhav-datt <madhav.datt@hotmail.com>
2016-05-22 17:58:49 +0530

    update email validation regex

--- src/send.py
+++ src/send.py
@@ -1,7 +1,8 @@
 #
 # send.py
 #
-# Send emails through SMTP
+# SendMail Class - a wrapper around the SMTP Python Package
+# Send emails from @gmail.com addresses
 #
 # Copyright (C)   2016    Madhav Datt
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -13,32 +14,44 @@ from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
 
-fromaddr = "YOUR EMAIL"
-toaddr = "EMAIL ADDRESS YOU SEND TO"
 
-msg = MIMEMultipart()
+class SendMail:
+    """
+    Wrapper around the Python SMTP Library for sending emails
+    """
 
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "SUBJECT OF THE EMAIL"
+    def __init__(self):
+        pass
 
-body = "TEXT YOU WANT TO SEND"
 
-msg.attach(MIMEText(body, 'plain'))
+    def send(self, username, password, to_email, cc_email, bcc_email, ):
 
-filename = "NAME OF THE FILE WITH ITS EXTENSION"
-attachment = open("PATH OF THE FILE", "rb")
-
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-msg.attach(part)
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, "YOUR PASSWORD")
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+# fromaddr = "YOUR EMAIL"
+# toaddr = "EMAIL ADDRESS YOU SEND TO"
+#
+# msg = MIMEMultipart()
+#
+# msg['From'] = fromaddr
+# msg['To'] = toaddr
+# msg['Subject'] = "SUBJECT OF THE EMAIL"
+#
+# body = "TEXT YOU WANT TO SEND"
+#
+# msg.attach(MIMEText(body, 'plain'))
+#
+# filename = "NAME OF THE FILE WITH ITS EXTENSION"
+# attachment = open("PATH OF THE FILE", "rb")
+#
+# part = MIMEBase('application', 'octet-stream')
+# part.set_payload((attachment).read())
+# encoders.encode_base64(part)
+# part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+#
+# msg.attach(part)
+#
+# server = smtplib.SMTP('smtp.gmail.com', 587)
+# server.starttls()
+# server.login(fromaddr, "YOUR PASSWORD")
+# text = msg.as_string()
+# server.sendmail(fromaddr, toaddr, text)
+# server.quit()
