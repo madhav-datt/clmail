76b87deeef821e72ed74aeb66e65350b22df13a1
madhav-datt <madhav.datt@hotmail.com>
2016-05-23 22:52:27 +0530

    add email sending wrapper

--- src/send.py
+++ src/send.py
@@ -31,3 +31,22 @@ class SendMail:
 
     def send(self, username, password, to, cc, bcc, ):
 
+
+    def login(self, password):
+        """
+        Login to the SMTP server using password. `login` only needs to be manually run when the
+        connection to the SMTP server was closed by the user.
+        """
+        self.smtp = smtplib.SMTP(self.host, self.port, **self.kwargs)
+        self.smtp.set_debuglevel(self.debuglevel)
+        if self.starttls:
+            self.smtp.ehlo()
+            if self.starttls is True:
+                self.smtp.starttls()
+            else:
+                self.smtp.starttls(**self.starttls)
+            self.smtp.ehlo()
+        self.is_closed = False
+        if not self.smtp_skip_login:
+            password = self._handle_password(password)
+            self.smtp.login(self.user, password)
