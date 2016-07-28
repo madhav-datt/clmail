aec9a5325c8b456ad865182d5b16b99bf86ec2a9
madhav-datt <madhav.datt@hotmail.com>
2016-07-28 11:20:30 +0530

    add setup.py file for adding to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='clmail',
+    packages=['clmail'],
+    version='0.1',
+    description='A random test lib',
+    author='Peter Downs',
+    author_email='peterldowns@gmail.com',
+    url='https://github.com/madhav-datt/clmail',
+    download_url='https://github.com/madhav-datt/clmail/tarball/1.0',
+    keywords=['testing', 'email', ''],
+    classifiers=[],
+)
